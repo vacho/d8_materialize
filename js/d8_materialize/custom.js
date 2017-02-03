80e3e3f8a4477b9b556564e7e8d84f0d8b76968c
vacho <vacho@koala-soft.com>
2017-02-03 19:18:49 -0400

    [theming] menu and tables

--- js/d8_materialize/custom.js
+++ js/d8_materialize/custom.js
@@ -1,3 +1,8 @@
 jQuery(document).ready(function() {
     jQuery('.button-collapse').sideNav();
+
+    // drop down sub-menu
+    jQuery('.menu-item--expanded').hover(function(){
+       jQuery(this).find('.sub-menu').toggle();
+    });
 });
