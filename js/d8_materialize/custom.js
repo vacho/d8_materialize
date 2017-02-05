df1ed90d4a6ef45a8bf75ee908ac7b8fd400d7a2
vacho <vacho@koala-soft.com>
2017-02-04 20:30:27 -0400

    [theming] formulario

--- js/d8_materialize/custom.js
+++ js/d8_materialize/custom.js
@@ -2,7 +2,15 @@ jQuery(document).ready(function() {
     jQuery('.button-collapse').sideNav();
 
     // drop down sub-menu
-    jQuery('.menu-item--expanded').hover(function(){
+    jQuery('.menu-item--expanded').hover(function() {
        jQuery(this).find('.sub-menu').toggle();
     });
+
+    if(window.location.pathname == '/dpf') {
+        jQuery('#block-d8-materialize-page-title').remove();
+    }
+
+    if(window.location.pathname == '/caja-de-ahorro') {
+        jQuery('#block-d8-materialize-page-title').remove();
+    }
 });
